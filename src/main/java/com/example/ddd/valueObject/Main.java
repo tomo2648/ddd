cc0bc071744db67e9e9023d6931a959932cd99f9
tomo8332 <suzuki.tomoyuki.8332@gmail.com>
2022-04-13 09:55:58 +0900

    20220413

--- src/main/java/com/example/ddd/valueObject/Main.java
+++ src/main/java/com/example/ddd/valueObject/Main.java
@@ -15,5 +15,26 @@ public class Main {
         FullName fullName2 = new FullName("john", "smith");
         System.out.println("" + fullName2.getFirstName());
 
+        // リスト2.7
+        String greet = "こんにちは";
+        System.out.println(greet);
+
+        greet = "Hello";
+        System.out.println(greet);
+
+        // リスト2.11
+        // 数字の変更
+        int num = 0;
+        num = 1;
+
+        // 文字の変更
+        String c = "0";
+        c = "b";
+
+        // 文字列の変更
+        String greet2 = "こんにちは";
+        greet2 = "Hello";
+
+
     }
 }
