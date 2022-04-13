fe94809c08b67529540e5e054e6bee5111531580
tomo8332 <suzuki.tomoyuki.8332@gmail.com>
2022-04-13 09:01:54 +0900

    gradle更新、サンプルソース追加、h2データベース接続環境準備

---
+++ src/main/java/com/example/ddd/valueObject/Main.java
@@ -0,0 +1,12 @@
+package com.example.ddd.valueObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String fullName = "naruse masanobu";
+        System.out.println(fullName);
+
+    }
+
+}
