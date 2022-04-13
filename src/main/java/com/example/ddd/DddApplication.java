b20f2c2d9a0d345d29c205b0c4c2be914c2207d1
nagano_keitaro <nknk9717[gmail.com>
2022-04-13 09:04:46 +0900

    教科書2.1コード

--- src/main/java/com/example/ddd/DddApplication.java
+++ src/main/java/com/example/ddd/DddApplication.java
@@ -8,6 +8,16 @@ public class DddApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DddApplication.class, args);
+
+		String fullName1 = "naruse masanobu";
+		System.out.println(fullName1);
+
+		String[] tokens = fullName1.split(" ");
+		System.out.println(tokens[0]);
+		System.out.println(tokens[1]);
+
+		FullName fullName2 = new FullName("john", "smith");
+		System.out.println("" + fullName2.getFirstName());
 	}
 
 }
